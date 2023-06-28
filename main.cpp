afff3f05702717337d72c02f05c07c8b8513169b
Jasper Hatilima <jasperhatilima@gmail.com>
2023-06-28 17:22:10 +0200

    Adding lambda function to main.cpp

--- main.cpp
+++ main.cpp
@@ -3,5 +3,12 @@
 
 int main()
 {
-    [out = std::ref(std::cout<<"Hello ")](){out.get()<<"World\n";}();
+	[out = std::ref(std::cout<<"Hello ")](){out.get()<<"World\n";}();
+	
+	auto sum = [](int a, int b)
+	{
+		return a + b;
+	}
+
+	std::cout<<"Sum of two intergers: "<<sum(5,6)<<std::endl;
 }
